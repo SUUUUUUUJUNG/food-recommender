da9820d712be0f6006541f89ca371899655e4571
SHIN SUJUNG <shinssjj79@gmail.com>
2024-12-30 11:55:24 +0900

    Initial commit

---
+++ src/main/java/com/foodrecommender/FoodRecommenderApplication.java
@@ -0,0 +1,13 @@
+package com.foodrecommender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodRecommenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodRecommenderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/foodrecommender/FoodRecommenderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foodrecommender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodRecommenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
